5fc01822efc75109d425c26d1f11a5fd02a9c510
roonie11lord <roonie11lord@gmail.com>
2019-03-22 17:40:08 +0100

    zmiana napisu

--- wykresy.py
+++ wykresy.py
@@ -18,7 +18,7 @@ z = -3*scipy.sqrt(1-(abs(x)/2)**0.5)
 pylab.fill_between(x, y, color="fuchsia")
 pylab.fill_between(x, z, color="fuchsia")
 pylab.xlim([-2.5, 2.5])
-pylab.text(0,-0.4, "Przepraszam Asiu",fontsize=26, fontweight='bold', color="white", horizontalalignment='center')
+pylab.text(0,-0.4, "Kocham Cię Asiu",fontsize=26, fontweight='bold', color="white", horizontalalignment='center')
 
 
 # In[ ]:
